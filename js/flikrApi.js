d7fc11fa3ad0048274bbb12fef76e0d2c1157102
Tyler Geerdts <tgeerdts@Tylers-MacBook-Pro.local>
2016-09-30 09:44:21 -0400

    Updated flikr and eventful API functions

--- js/flikrApi.js
+++ js/flikrApi.js
@@ -1,12 +1,9 @@
-
-
- function getPhotoFromFlikr(city){
-	var queryUrl="https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=8f7b31512dd196d30b1af5847ccfcb58&tags="+city+"%2C+skyline&tag_mode=all&text="+city+"&accuracy=11&per_page=1&page=1&format=json&nojsoncallback=1"
+function getPhotoFromFlikr(city) {
+	var queryUrl="https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=8f7b31512dd196d30b1af5847ccfcb58&tags="+city+"%2C+skyline&tag_mode=all&text="+city+"&accuracy=11&per_page=1&page=1&format=json&nojsoncallback=1";
 	$.ajax({url:queryUrl,method:"GET"}).done(function(response){
-		console.log(response)
-    var pictureUrl= "https://farm"+response.photos.photo[0].farm+".staticflickr.com/"+response.photos.photo[0].server+"/"+response.photos.photo[0].id+"_"+response.photos.photo[0].secret+".jpg"
-    console.log(pictureUrl)
+    var pictureUrl= "https://farm"+response.photos.photo[0].farm+".staticflickr.com/"+response.photos.photo[0].server+"/"+response.photos.photo[0].id+"_"+response.photos.photo[0].secret+".jpg";
+    $('#header').css('background-image', 'url(' + pictureURL + ')');
 	});
 }
 
-getPhotoFromFlikr('New York');
+// getPhotoFromFlikr('New York');
